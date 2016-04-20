fdb23c019419a367559e0cd975d7ab99ccd96c8e
Tom Zille <tom.zille@gmail.com>
2016-04-20 11:13:47 +0200

    gulp minify and uglified integrated

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var miniCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
 
 gulp.task('default',['imageconverter','linting','minify']);
 
@@ -13,4 +17,21 @@ gulp.task('linting', function() {
 
 gulp.task('minify', function() {
 	console.log('minify task started')
+	
+	gulp.src('src/js/*.js')
+	.pipe(concat('app.js'))
+	.pipe(uglify())
+	.pipe(rename('app.min.js'))
+	.pipe(gulp.dest('build/js/'));
+	
+	console.log('JS Uglified');
+	
+	gulp.src('src/css/*.css')
+	.pipe(miniCss())
+	.pipe(rename({
+		suffix: '.min'
+	}))
+	.pipe(gulp.dest('build/css'));
+	
+	console.log('CSS minified');
 });
